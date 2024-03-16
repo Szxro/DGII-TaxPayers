d2af8b0d4c18f8013479fa86c07050e5bcac2ffe
Szxro <szxro@gmail.com>
2024-03-16 18:15:06 -0400

    Add event method and list of event in the base entity

--- DGII-Taxpayers.Domain/Entitites/Common/BaseEntity.cs
+++ DGII-Taxpayers.Domain/Entitites/Common/BaseEntity.cs
@@ -1,6 +1,27 @@
-﻿namespace DGII_Taxpayers.Domain.Entitites.Common;
+﻿using DGII_Taxpayers.Domain.Contracts;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DGII_Taxpayers.Domain.Entitites.Common;
 
 public abstract class BaseEntity
 {
     public int Id { get; set; }
+
+    [NotMapped]
+    protected List<IEvent> events = new List<IEvent>();
+
+    public void AddEvent(IEvent @event)
+    {
+        events.Add(@event);
+    }
+
+    public void RemoveEvent(IEvent @event)
+    {
+        events.Remove(@event);
+    }
+
+    public void ClearEvents()
+    {
+        events.Clear();
+    }
 }
