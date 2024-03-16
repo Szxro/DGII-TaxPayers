b7f88a9ec62ba44ad2dd3d81c258471d49b75462
Szxro <szxro@gmail.com>
2024-03-16 18:42:14 -0400

    Add database options class

---
+++ DGII-Taxpayers.Infrastructure/Options/DatabaseOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace DGII_Taxpayers.Infrastructure.Options;
+
+internal class DatabaseOptions
+{
+    public const string sectionName = "DatabaseOptions";
+
+    public string? ConnectionString { get; init; }
+
+    public int MaxRetryCount { get; init; }
+
+    public int CommandTimeout { get; init; }
+
+    public bool EnableDetailedErrors { get; init; }
+
+    public bool EnableSensitiveDataLogging { get; init; }
+}
