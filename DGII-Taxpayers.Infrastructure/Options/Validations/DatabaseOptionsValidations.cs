4c060619d83f9f21517a63cd9aa60c78e791e191
Szxro <szxro@gmail.com>
2024-03-16 20:07:50 -0400

    Add validations for the database options class

---
+++ DGII-Taxpayers.Infrastructure/Options/Validations/DatabaseOptionsValidations.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+
+namespace DGII_Taxpayers.Infrastructure.Options.Validations;
+
+internal class DatabaseOptionsValidations : AbstractValidator<DatabaseOptions>
+{
+    public DatabaseOptionsValidations()
+    {
+        RuleFor(options => options.ConnectionString).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(options => options.ConnectionString).NotNull().WithMessage("The {PropertyName} cant be null");
+
+        RuleFor(options => options.MaxRetryCount).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(options => options.MaxRetryCount).NotNull().WithMessage("The {PropertyName} cant be null");
+
+        RuleFor(options => options.MaxRetryCount).Must(count => count < int.MinValue).WithMessage("The {PropertyName} cant be zero or lower than zero");
+
+        RuleFor(options => options.CommandTimeout).NotNull().WithMessage("The {PropertyName} cant be null");
+
+        RuleFor(options => options.CommandTimeout).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(options => options.MaxRetryCount).Must(count => count < int.MinValue).WithMessage("The {PropertyName} cant be zero or lower than zero");
+
+        RuleFor(options => options.EnableDetailedErrors).NotNull().WithMessage("The {PropertyName} cant be null");
+
+        RuleFor(options => options.EnableDetailedErrors).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(options => options.EnableSensitiveDataLogging).NotNull().WithMessage("The {PropertyName} cant be null");
+
+        RuleFor(options => options.EnableSensitiveDataLogging).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+    }
+}
