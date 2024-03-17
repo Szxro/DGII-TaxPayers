3aeccde0e39b2563d5a912e1c92f8c51ced6a28d
Szxro <szxro@gmail.com>
2024-03-16 20:13:38 -0400

    Add db context

---
+++ DGII-Taxpayers.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DGII_Taxpayers.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace DGII_Taxpayers.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<TaxPayer> TaxPayer => Set<TaxPayer>();
+
+    public DbSet<TaxReceipt> TaxReceipt => Set<TaxReceipt>();
+
+    public DbSet<PersonType> PersonType => Set<PersonType>();
+}
