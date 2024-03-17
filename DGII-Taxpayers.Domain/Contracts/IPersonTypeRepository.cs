80a3700c50295ad3ed5881fd4668ee950a3eed46
Szxro <szxro@gmail.com>
2024-03-16 20:13:02 -0400

    Add repository and interface for the PersonType entity

---
+++ DGII-Taxpayers.Domain/Contracts/IPersonTypeRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace DGII_Taxpayers.Domain.Contracts;
+
+public interface IPersonTypeRepository
+{
+    Task AddDefaultPersonType();
+}
---
+++ DGII-Taxpayers.Infrastructure/Repositories/PersonTypeRepository.cs
@@ -0,0 +1,31 @@
+﻿using DGII_Taxpayers.Domain.Contracts;
+using DGII_Taxpayers.Domain.Entitites;
+using DGII_Taxpayers.Infrastructure.Common;
+using DGII_Taxpayers.Infrastructure.Persistence;
+
+namespace DGII_Taxpayers.Infrastructure.Repositories;
+
+public class PersonTypeRepository 
+    : GenericRepository<PersonType>, IPersonTypeRepository
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public PersonTypeRepository(AppDbContext dbContext,
+                                IUnitOfWork unitOfWork) : base(dbContext)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task AddDefaultPersonType()
+    {
+        ICollection<PersonType> personTypes = new HashSet<PersonType>()
+        {
+            new PersonType(){TypeName = "PERSONA JURIDICA" },
+            new PersonType(){TypeName = "PERSONA FISICA" },
+        };
+
+        AddRange(personTypes);
+
+        await _unitOfWork.SaveChangesAsync();
+    }
+}
