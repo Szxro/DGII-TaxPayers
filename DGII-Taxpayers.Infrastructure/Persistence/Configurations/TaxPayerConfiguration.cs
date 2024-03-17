81783b7da2c9943ee958821ea49ea3434bea2ad5
Szxro <szxro@gmail.com>
2024-03-16 20:14:49 -0400

    Add db context configurations for the entities

---
+++ DGII-Taxpayers.Infrastructure/Persistence/Configurations/TaxPayerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using DGII_Taxpayers.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DGII_Taxpayers.Infrastructure.Persistence.Configurations;
+
+public class TaxPayerConfiguration : IEntityTypeConfiguration<TaxPayer>
+{
+    public void Configure(EntityTypeBuilder<TaxPayer> builder)
+    {
+        builder.HasIndex(x => x.RncID).IsUnique();
+    }
+}
---
+++ DGII-Taxpayers.Infrastructure/Persistence/Configurations/TaxReceiptConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using DGII_Taxpayers.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DGII_Taxpayers.Infrastructure.Persistence.Configurations;
+
+public class TaxReceiptConfiguration : IEntityTypeConfiguration<TaxReceipt>
+{
+    public void Configure(EntityTypeBuilder<TaxReceipt> builder)
+    {
+        builder.HasIndex(x => x.NCF).IsUnique();
+    }
+}
