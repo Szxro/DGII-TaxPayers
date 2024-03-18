4a91b9ee7df177d31f38ac7eb8836fafbc23c4c6
Szxro <szxro@gmail.com>
2024-03-17 22:18:58 -0400

    Make some change in the PersonTypeConfiguration

---
+++ DGII-Taxpayers.Infrastructure/Persistence/Configurations/PersonTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using DGII_Taxpayers.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DGII_Taxpayers.Infrastructure.Persistence.Configurations;
+
+public class PersonTypeConfiguration : IEntityTypeConfiguration<PersonType>
+{
+    public void Configure(EntityTypeBuilder<PersonType> builder)
+    {
+        builder.HasIndex(x => x.TypeName).IsUnique();
+    }
+}
