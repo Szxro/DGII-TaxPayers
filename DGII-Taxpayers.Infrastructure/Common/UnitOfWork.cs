fd8a02daf5af2e1f87feae5caee86b5ce830a931
Szxro <szxro@gmail.com>
2024-03-17 22:15:24 -0400

    Make some changes in the UnitOfWork to dispatch entities events

--- DGII-Taxpayers.Infrastructure/Common/UnitOfWork.cs
+++ DGII-Taxpayers.Infrastructure/Common/UnitOfWork.cs
@@ -1,5 +1,7 @@
 ﻿using DGII_Taxpayers.Domain.Contracts;
+using DGII_Taxpayers.Domain.Entitites.Common;
 using DGII_Taxpayers.Infrastructure.Persistence;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace DGII_Taxpayers.Infrastructure.Common;
@@ -7,10 +9,14 @@ namespace DGII_Taxpayers.Infrastructure.Common;
 public class UnitOfWork : IUnitOfWork
 {
     private readonly AppDbContext _appDbContext;
+    private readonly IPublisher _publisher;
 
-    public UnitOfWork(AppDbContext appDbContext)
+    public UnitOfWork(
+        AppDbContext appDbContext,
+        IPublisher publisher)
     {
         _appDbContext = appDbContext;
+        _publisher = publisher;
     }
 
     public void ChangeContextTrackerToUnchanged(object entity)
@@ -20,6 +26,19 @@ public class UnitOfWork : IUnitOfWork
 
     public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        return await _appDbContext.SaveChangesAsync(cancellationToken);
+        IEnumerable<IEvent> events = _appDbContext.ChangeTracker.Entries<BaseEntity>()
+                                    .Select(e => e.Entity)
+                                    .Where(e => e.DomainEvents.Any())
+                                    .SelectMany(e => e.DomainEvents)
+                                    .ToArray();
+
+        var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+
+        foreach (IEvent domainEvent in events)
+        {
+            await _publisher.Publish(domainEvent, cancellationToken);
+        }
+
+        return result;
     }
 }
