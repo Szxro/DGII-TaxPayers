65ebb947de6a9175a6d398f39607c0fc7e39b98f
Szxro <szxro@gmail.com>
2024-03-17 22:22:15 -0400

    Add required method for the repositories

---
+++ DGII-Taxpayers.Domain/Contracts/ITaxPayerRepository.cs
@@ -0,0 +1,18 @@
+﻿using DGII_Taxpayers.Domain.DTOs;
+using DGII_Taxpayers.Domain.Entitites;
+
+namespace DGII_Taxpayers.Domain.Contracts;
+
+public interface ITaxPayerRepository
+{
+    void CreateTaxPayerAsync(string rncId,
+                             string name,
+                             PersonType personType,
+                             string status);
+
+    Task<bool> IsTaxPayerRncIdNotAvaliable(string rncID);
+
+    Task<List<TaxPayerDTO>> GetAllTaxPayer();
+
+    Task<TaxPayer?> GetTaxPayerByRncId(string rncId);
+}
---
+++ DGII-Taxpayers.Domain/Contracts/ITaxReceiptRepository.cs
@@ -0,0 +1,17 @@
+﻿using DGII_Taxpayers.Domain.DTOs;
+using DGII_Taxpayers.Domain.Entitites;
+
+namespace DGII_Taxpayers.Domain.Contracts;
+
+public interface ITaxReceiptRepository
+{
+    void CreateTaxReceipt(TaxPayer taxPayer,
+                          string ncf,
+                          string amount);
+
+    Task<bool> IsNcfNotAvaliable(string ncf);
+
+    Task<List<TaxReceiptDTO>> GetAllTaxReceipt();
+
+    Task<List<TaxReceiptDTO>> GetTaxReceiptsByRncId(string rncId);
+}
