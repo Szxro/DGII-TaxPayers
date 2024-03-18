3a97a5e40f70fc97a9899e2f77c809afdc167596
Szxro <szxro@gmail.com>
2024-03-17 22:31:20 -0400

    Add Validation Exception

---
+++ DGII-Taxpayers.Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,19 @@
+﻿using DGII_Taxpayers.Domain.Core.Primitives;
+
+namespace DGII_Taxpayers.Application.Common.Exceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException()
+        : base("One or more validation failures have occurred.")
+    {
+        Errors = new List<Error[]>();
+    }
+    public IReadOnlyCollection<Error[]> Errors { get; }
+
+    public ValidationException(Error[] validationErrors)
+        : this()
+    {
+        Errors = new List<Error[]>() { validationErrors }; 
+    }
+}
