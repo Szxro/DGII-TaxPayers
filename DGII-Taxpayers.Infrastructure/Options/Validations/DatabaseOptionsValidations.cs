40b08091db0463c23dddd2d7195f702ccdb1a37d
Szxro <szxro@gmail.com>
2024-03-17 22:21:21 -0400

    Add some changes in the DatabaseOptionsValidations

--- DGII-Taxpayers.Infrastructure/Options/Validations/DatabaseOptionsValidations.cs
+++ DGII-Taxpayers.Infrastructure/Options/Validations/DatabaseOptionsValidations.cs
@@ -2,7 +2,7 @@
 
 namespace DGII_Taxpayers.Infrastructure.Options.Validations;
 
-internal class DatabaseOptionsValidations : AbstractValidator<DatabaseOptions>
+public class DatabaseOptionsValidations : AbstractValidator<DatabaseOptions>
 {
     public DatabaseOptionsValidations()
     {
@@ -10,21 +10,25 @@ internal class DatabaseOptionsValidations : AbstractValidator<DatabaseOptions>
 
         RuleFor(options => options.ConnectionString).NotNull().WithMessage("The {PropertyName} cant be null");
 
+
         RuleFor(options => options.MaxRetryCount).NotEmpty().WithMessage("The {PropertyName} cant be empty");
 
         RuleFor(options => options.MaxRetryCount).NotNull().WithMessage("The {PropertyName} cant be null");
 
-        RuleFor(options => options.MaxRetryCount).Must(count => count < int.MinValue).WithMessage("The {PropertyName} cant be zero or lower than zero");
+        RuleFor(options => options.MaxRetryCount).Must(count => count > 0).WithMessage("The {PropertyName} cant be zero or lower than zero");
+
 
         RuleFor(options => options.CommandTimeout).NotNull().WithMessage("The {PropertyName} cant be null");
 
         RuleFor(options => options.CommandTimeout).NotEmpty().WithMessage("The {PropertyName} cant be empty");
 
-        RuleFor(options => options.MaxRetryCount).Must(count => count < int.MinValue).WithMessage("The {PropertyName} cant be zero or lower than zero");
+        RuleFor(options => options.CommandTimeout).Must(count => count > 0).WithMessage("The {PropertyName} cant be zero or lower than zero");
+
 
         RuleFor(options => options.EnableDetailedErrors).NotNull().WithMessage("The {PropertyName} cant be null");
 
         RuleFor(options => options.EnableDetailedErrors).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+
 
         RuleFor(options => options.EnableSensitiveDataLogging).NotNull().WithMessage("The {PropertyName} cant be null");
 
