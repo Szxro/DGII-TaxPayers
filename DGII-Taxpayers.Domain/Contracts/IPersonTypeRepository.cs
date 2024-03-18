ac17d0cee0002500a7f2d13df39316d8e9b4156d
Szxro <szxro@gmail.com>
2024-03-17 22:06:49 -0400

    Add new methods for the person type interface

--- DGII-Taxpayers.Domain/Contracts/IPersonTypeRepository.cs
+++ DGII-Taxpayers.Domain/Contracts/IPersonTypeRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace DGII_Taxpayers.Domain.Contracts;
+﻿using DGII_Taxpayers.Domain.DTOs;
+using DGII_Taxpayers.Domain.Entitites;
+
+namespace DGII_Taxpayers.Domain.Contracts;
 
 public interface IPersonTypeRepository
 {
     Task AddDefaultPersonType();
+
+    void CreatePersonType(string typeName);
+
+    Task<bool> IsPersonTypeNameFound(string typeName);
+
+    Task<PersonType?> GetPersonTypeByTypeName(string typeName);
+
+    Task<List<PersonTypeDTO>> GetAllPersonType();
 }
