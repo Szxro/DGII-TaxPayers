39b4ad8c5d28424d5eede880622a24ce819d1ad4
Szxro <szxro@gmail.com>
2024-03-17 22:32:03 -0400

    Add service registration for the application layer

---
+++ DGII-Taxpayers.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using DGII_Taxpayers.Application.Common.Behaviors;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DGII_Taxpayers.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+
+            config.AddOpenBehavior(typeof(ExceptionHandlingPipelineBehavior<,>));
+            config.AddOpenBehavior(typeof(RequestPerfomancePipelineBehavior<,>));
+            config.AddOpenBehavior(typeof(RequestLoggingPipelineBehavior<,>));
+            config.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
+            config.AddOpenBehavior(typeof(QueryCachingPipelineBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
