725e1878fa3e6f13c7fc04c95a70476a4f90942f
Szxro <szxro@gmail.com>
2024-03-17 22:05:58 -0400

    Add interface and service for the CacheService

---
+++ DGII-Taxpayers.Domain/Contracts/ICaheService.cs
@@ -0,0 +1,15 @@
+﻿namespace DGII_Taxpayers.Domain.Contracts;
+
+public interface ICaheService
+{
+    //Task<T> GetOrCreateAsync<T>(string key,
+    //                            Func<CancellationToken, Task<T>> factory,
+    //                            TimeSpan? timeSpan = null,
+    //                            CancellationToken cancellationToken = default);
+
+    T? GetSync<T>(string key);
+
+    void SetSync<T>(string key,T value,TimeSpan timeSpan);
+
+    void RemoveSync(string key);
+}
---
+++ DGII-Taxpayers.Infrastructure/Services/CacheService.cs
@@ -0,0 +1,35 @@
+﻿using DGII_Taxpayers.Domain.Contracts;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DGII_Taxpayers.Infrastructure.Services;
+
+public class CacheService : ICaheService
+{
+    private readonly IMemoryCache _memoryCache;
+
+    public CacheService(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+    public T? GetSync<T>(string key)
+    {
+        return _memoryCache.Get<T>(key);
+    }
+
+    public void RemoveSync(string key)
+    {
+        bool result = _memoryCache.TryGetValue(key, out _);
+
+        if (!result)
+        {
+            return;
+        }
+
+        _memoryCache.Remove(key);
+    }
+
+    public void SetSync<T>(string key,T value, TimeSpan timeSpan)
+    {
+        _memoryCache.Set(key, value, timeSpan);
+    }
+}
