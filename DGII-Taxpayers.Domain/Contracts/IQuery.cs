89c90ce8e4bbefc4b6ed571d6d71efd30c315853
Szxro <szxro@gmail.com>
2024-03-17 22:09:29 -0400

    Update IQuery interface and create ICacheQuery interface (Non-Generic / Generic)

--- DGII-Taxpayers.Domain/Contracts/IQuery.cs
+++ DGII-Taxpayers.Domain/Contracts/IQuery.cs
@@ -3,3 +3,12 @@
 namespace DGII_Taxpayers.Domain.Contracts;
 
 public interface IQuery<out TResponse> : IRequest<TResponse> { }
+
+public interface ICachedQuery<out TResponse> : IQuery<TResponse> , ICachedQuery{ }
+
+public interface ICachedQuery
+{
+    string CahedKey { get; }
+
+    TimeSpan Expiration { get; }
+}
